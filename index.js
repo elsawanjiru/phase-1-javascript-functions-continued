4fb1c4fc5bcd109a12b77b87a55d61fa16597854
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-06-15 13:18:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    if (activity === undefined) {
+        activity = 'roller skate';
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+def mondayWork(activity = 'go to the office') {
+    if (activity === undefined) {
+        activity = 'go to the office';
+    }
+    return 'This Monday, I will ${activity}.';
+}
+
+function wrapAdjective(flair = '*'){
+    return function(adjective) {
+        return'you are ${flair}${adjective}${flair}!';
+    }
+}
