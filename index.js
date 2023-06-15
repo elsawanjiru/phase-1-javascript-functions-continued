0771648c09c0a39fad02bfcfb362e60484003cce
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-06-15 13:50:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // code your solution here
-function saturdayFun(activity = 'roller-skate') {
-    if (activity === undefined) {
-        activity = 'roller skate';
-    }
+function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
-}
-
-
-def mondayWork(activity = 'go to the office') {
-    if (activity === undefined) {
-        activity = 'go to the office';
-    }
-    return 'This Monday, I will ${activity}.';
-}
-
-function wrapAdjective(flair = '*'){
-    return function(adjective) {
-        return'you are ${flair}${adjective}${flair}!';
-    }
-}
+  }
+  
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  function wrapAdjective(symbol) {
+    return function (adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
+  console.log(saturdayFun());
+  console.log(saturdayFun("bathe my dog")); 
+  
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"));
+  
+  const wrapWithStars = wrapAdjective('*');
+  console.log(wrapWithStars("a hard worker"));
+  
+  const wrapWithBars = wrapAdjective("||");
+  console.log(wrapWithBars("a dedicated programmer"));
